95d451b3634aa92e038db3e4d4285df06aa03ab7
Rafael Reis <rafa.araujoreis@gmail.com>
2023-06-02 08:25:13 -0300

    feat: add component card

---
+++ projects/card/src/lib/card.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ui-card',
+  template: `
+    <p>
+      card works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class CardComponent {
+
+}
